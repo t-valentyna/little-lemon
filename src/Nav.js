0766d7ea7e4180b5a2956e5f9bae049b2dc0e9ed
Trots Valentyna <valentinochkat16@gmail.com>
2024-02-13 19:04:18 +0200

    Setting up the CSS layout

--- src/Nav.js
+++ src/Nav.js
@@ -3,6 +3,7 @@ function Nav() {
       <nav>
         <ul>
             <li><a>Home</a></li>
+            <li><a>About</a></li>
             <li><a>Menu</a></li>
             <li><a>Reservations</a></li>
             <li><a>Order online</a></li>
