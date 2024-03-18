0b9e1b9cc8dc383f1b184072b5b3fb3d1a1aa1ee
Trots Valentyna <valentinochkat16@gmail.com>
2024-03-18 18:08:36 +0200

    Burger menu was added for mobile screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import Nav from './Nav';
 import Logo from '../Logo.svg';
+import Hamburger from '../assets/hamburger-menu.svg';
 
 function Header() {
     return (
       <header>
         <img src={Logo} alt="Logo" />
-        <Nav></Nav>
+        <div id="nav-wrapper" tabIndex={0}>
+          <button id="hamburger" ><img src={Hamburger} alt="hamburger menu" /></button>
+          <Nav></Nav>
+        </div>
       </header>
     );
   }
